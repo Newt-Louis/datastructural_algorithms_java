b54ac9df688655b474deeb03992e08e471fd992c
Newt Louis <l.ngoc.17395@gmail.com>
2024-12-08 19:54:25 +0700

    complete lesson about insertion sort

---
+++ src/main/java/com/structural_algorithm/mavenproject1/SearchAlgorithms.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.structural_algorithm.mavenproject1;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SearchAlgorithms {
+
+    public static int[] insertionSort1(int arr[]){
+        if (arr.length <= 1) {
+            return arr;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            while (j>0 && arr[j] <= arr[j-1]) {
+                int tempt = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = tempt;
+                j--;
+            }
+        }
+        return arr;
+    }
+    
+    public static int[] insertionSort2(int arr[]){
+        if (arr.length <= 1) {
+            return arr;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && key <= arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        return arr;
+    }
+}
---
+++ src/main/java/com/structural_algorithm/mavenproject1/index.java
@@ -0,0 +1,12 @@
+package com.structural_algorithm.mavenproject1;
+
+import java.util.Arrays;
+
+public class index {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int[] arr = {6,16,5,28,30,9};
+        System.out.println(Arrays.toString(SearchAlgorithms.insertionSort2(arr)));
+    }
+}
