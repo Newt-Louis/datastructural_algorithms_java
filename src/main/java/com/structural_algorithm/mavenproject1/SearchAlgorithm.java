ca1bccb2616b19a8d002f13106b99d1e8ed28061
Newt Louis <l.ngoc.17395@gmail.com>
2024-12-09 00:13:13 +0700

    change file name, split lession, learn and use generic type for search algorithm

---
+++ src/main/java/com/structural_algorithm/mavenproject1/SearchAlgorithm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.structural_algorithm.mavenproject1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SearchAlgorithm {
+    public static <T> T[] binarySearch(T[] arr){
+        return arr;
+    }
+    
+}
